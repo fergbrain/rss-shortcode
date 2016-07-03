9b27d8795169faa4729a35ec54f1086c4264e01e
Andrew Ferguson <andrew@fergcorp.com>
2016-07-03 08:42:00 -0700

    HTML handling
    Better handles HTML now

--- rss-shortcode.php
+++ rss-shortcode.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: RSS Shortcode
-Version: 0.2
+Plugin Name: RSS Shortcode (ft. Fergcorp)
+Version: 0.3
 Plugin URI: http://yoast.com/wordpress/rss-shortcode/
 Description: Makes it easy to display an RSS feed on a page
-Author: Joost de Valk
+Author: Joost de Valk and Andrew Ferguson
 Author URI: http://yoast.com/
 */
 
@@ -15,7 +15,17 @@ function yoast_rss_shortcode( $atts ) {
 		"excerpt" 	=> true,
 		"target"	=> '_self'
 	), $atts));
-	require_once(ABSPATH.WPINC.'/rss.php');  
+	require_once(ABSPATH.WPINC.'/rss.php');
+	$allowed_html = array(
+    				'a' => array(
+        					'href' => array(),
+        					'title' => array()
+    					),
+    				'br' => array(),
+    				'em' => array(),
+    				'strong' => array(),
+				'p' => array(),
+			);
 	if ( $feed != "" && $rss = fetch_rss( $feed ) ) {
 		$content = '<ul>';
 		if ( $num !== -1 ) {
@@ -24,11 +34,11 @@ function yoast_rss_shortcode( $atts ) {
 		foreach ( (array) $rss->items as $item ) {
 			$content .= '<li>';
 			if ($target != '_self')
-				$content .= '<a href="'.esc_url( $item['link'] ).'" target="'.esc_attr($target).'">'. esc_html($item['title']) .'</a>';
+				$content .= '<a href="'.esc_url( $item['link'] ).'" target="'.esc_attr($target).'">'. wp_kses($item['title'], $allowed_html) .'</a>';
 			else
-				$content .= '<a href="'.esc_url( $item['link'] ).'">'. esc_html($item['title']) .'</a>';
+				$content .= '<a href="'.esc_url( $item['link'] ).'">'. wp_kses($item['title'], $allowed_html) .'</a>';
 			if ( $excerpt != false && $excerpt != "false") {
-				$content .= '<br/><span class="rss_excerpt">'. esc_html($item['summary']) .'</span>';
+				$content .= '<br/><span class="rss_excerpt">'. wp_kses($item['summary'], $allowed_html) .'</span>';
 			}
 			$content .= '</li>';
 		}
